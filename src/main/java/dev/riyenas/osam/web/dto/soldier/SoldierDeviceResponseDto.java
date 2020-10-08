808e1af8222f76cf31571e4a9193d767a2798f1c
riyenas0925 <riyenas0925@gmail.com>
2020-10-08 22:58:59 +0900

    ENH : 병사 정보 전체 조회 기능 구현
    * 전체 병사 정보를 조회 하는 기능 구현
    * 병사중 모바일 기기가 2개 이상이면 병사A - 모바일 기기1, 병사A - 모바일 기기2 와 같이 전부 조회 한다.
    
    Resolves #23

---
+++ src/main/java/dev/riyenas/osam/web/dto/soldier/SoldierDeviceResponseDto.java
@@ -0,0 +1,36 @@
+package dev.riyenas.osam.web.dto.soldier;
+
+import dev.riyenas.osam.domain.device.Device;
+import dev.riyenas.osam.domain.soldier.Soldier;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class SoldierDeviceResponseDto {
+    private Long soldierId;
+    private String serviceNumber;
+    private String name;
+    private String rank;
+    private String unit;
+    private Long deviceId;
+    private String serialNumber;
+    private String type;
+    private String manufacturer;
+    private String phoneNumber;
+    private String uuid;
+
+    public SoldierDeviceResponseDto(Soldier soldier, Device device) {
+        this.soldierId = soldier.getId();
+        this.serviceNumber = soldier.getServiceNumber();
+        this.name = soldier.getName();
+        this.rank = soldier.getRank();
+        this.unit = soldier.getUnit();
+        this.deviceId = device.getId();
+        this.serialNumber = device.getSerialNumber();
+        this.type = device.getType();
+        this.manufacturer = device.getManufacturer();
+        this.phoneNumber = device.getPhoneNumber();
+        this.uuid = device.getUuid();
+    }
+}
