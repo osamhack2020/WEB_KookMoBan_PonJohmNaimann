4639af5d85e08e85e446ec1009a986fb93e43dda
riyenas0925 <riyenas0925@gmail.com>
2020-10-21 18:04:19 +0900

    ENH : 휴대폰 반납 현황 관리 페이지
    * 전체 반납 현황을 조회하는 페이지 (log/all)
    * 비정상 반납 현황을 조회하는 페이지 (log/fault)
    * static resources 경로 수정
    
    Resolves #68

--- src/main/java/dev/riyenas/osam/web/dto/returnLog/ReturnLogResponseDto.java
+++ src/main/java/dev/riyenas/osam/web/dto/returnLog/ReturnLogResponseDto.java
@@ -1,8 +1,9 @@
 package dev.riyenas.osam.web.dto.returnLog;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import dev.riyenas.osam.domain.device.Device;
 import dev.riyenas.osam.domain.log.ReturnLog;
-import dev.riyenas.osam.web.dto.device.DeviceResponseDto;
+import dev.riyenas.osam.domain.soldier.Soldier;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -12,20 +13,33 @@ import java.util.Date;
 @ToString
 public class ReturnLogResponseDto {
     private Long id;
+    private String serviceNumber;
+    private String name;
     private Date returnTime;
     private Double weight;
     private String state;
-    private DeviceResponseDto device;
+    private String serialNumber;
+    private String type;
+    private String manufacturer;
+    private String phoneNumber;
 
     @JsonIgnore
     private String photo;
 
     public ReturnLogResponseDto(ReturnLog returnLog) {
+        Soldier soldier = returnLog.getDevice().getSoldier();
+        Device device = returnLog.getDevice();
+
+        this.serviceNumber = soldier.getServiceNumber();
+        this.name = soldier.getName();
         this.id = returnLog.getId();
         this.returnTime = returnLog.getReturnTime();
         this.photo = returnLog.getPhoto();
         this.weight = returnLog.getWeight();
         this.state = returnLog.getState().toString();
-        this.device = new DeviceResponseDto(returnLog.getDevice());
+        this.serialNumber = device.getSerialNumber();
+        this.type = device.getType();
+        this.manufacturer = device.getManufacturer();
+        this.phoneNumber = device.getPhoneNumber();
     }
 }
