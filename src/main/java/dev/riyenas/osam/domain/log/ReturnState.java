d2bd11a9476a11e574c96c4a31fc1621a1ca4b59
riyenas0925 <riyenas0925@gmail.com>
2020-10-23 23:38:37 +0900

    ENH : 납 현황에서 반납상태순으로 정렬해서 보여주는 기능
    * 미반납, 상태 이상, 반납 순서로 표시
    * 실시간 반납 정보 메인 화면(/)으로 이동
    
    Resolves #82

--- src/main/java/dev/riyenas/osam/domain/log/ReturnState.java
+++ src/main/java/dev/riyenas/osam/domain/log/ReturnState.java
@@ -1,18 +1,20 @@
 package dev.riyenas.osam.domain.log;
 
 public enum ReturnState {
-    PASS("정상반납", "bg-success"),
-    NON_PASS("미반납", "bg-danger"),
-    TIME_DELAY("지연반납", "bg-warning"),
-    WEIGHT_FAULT("무게이상", "bg-warning");
+    PASS("정상반납", "bg-success", 4L),
+    NON_PASS("미반납", "bg-danger", 1L),
+    TIME_DELAY("지연반납", "bg-warning", 2L),
+    WEIGHT_FAULT("무게이상", "bg-warning", 3L);
 
-    ReturnState(String state, String icon) {
+    ReturnState(String state, String icon, Long order) {
         this.state = state;
         this.icon = icon;
+        this.order = order;
     }
 
     private final String state;
     private final String icon;
+    private final Long order;
 
     public String getState() {
         return state;
@@ -20,5 +22,9 @@ public enum ReturnState {
 
     public String getIcon() {
         return icon;
+    }
+
+    public Long getOrder() {
+        return order;
     }
 }
