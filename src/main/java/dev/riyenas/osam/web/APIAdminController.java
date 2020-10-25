2ec206e24cb17c33135a07ddbd93d5df75b9b32e
riyenas0925 <riyenas0925@gmail.com>
2020-10-25 16:20:14 +0900

    ENH : 유효한 회원가입코드인지 확인하는 API 생성
    * 유효하다면 관리자 이름과 소속을 반환
    * 유효하지 Exception Message 반환
    
    Resolves #97

--- src/main/java/dev/riyenas/osam/web/APIAdminController.java
+++ src/main/java/dev/riyenas/osam/web/APIAdminController.java
@@ -1,10 +1,12 @@
 package dev.riyenas.osam.web;
 
 import dev.riyenas.osam.service.AdminService;
+import dev.riyenas.osam.web.dto.admin.AdminResponseDto;
 import dev.riyenas.osam.web.dto.admin.AdminRuleResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +23,10 @@ public class APIAdminController {
     @GetMapping("find/all")
     public List<AdminRuleResponseDto> findAll() {
         return adminService.findAll();
+    }
+
+    @GetMapping("find/signUpCode/{signUpCode}")
+    public AdminResponseDto findSignUpCode(@PathVariable String signUpCode) {
+        return adminService.findBySignUpCode(signUpCode);
     }
 }
